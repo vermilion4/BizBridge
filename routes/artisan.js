bfadb34650ff2c25873ae75455a599c3d6256a83
Ndupu Adaeze <andupu4@gmail.com>
2020-12-10 14:04:03 +0100

    commited

--- routes/artisan.js
+++ routes/artisan.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const passport = require('passport');
-var AWS = require('aws-sdk');
 
 
 let fs = require('fs');
@@ -127,21 +126,7 @@ var myKey =  encodeURIComponent('jpg');
             password2,
             upload:{
               // data:fs.readFileSync(path.join(__dirname+'/uploads/'+req.file.filename)),
-              data: fs.readFile(path.join('uploads/'+req.file.filename), function (err, data) {
-  if (err) { throw err; }
-
-     params = {Bucket: myBucket, Key: myKey, Body: data };
-     s3.putObject(params, function(err, data) {
-         if (err) {
-             console.log(err)
-
-         } else {
-
-             console.log("Successfully uploaded data to myBucket/myKey");
-         }
-
-      })
-    }),
+              data: fs.readFile(path.join('uploads/'+req.file.filename)),
             contentType: 'image/png'
             }
                 })
